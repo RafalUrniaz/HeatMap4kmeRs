bd737c2aff28ba817b1a07e2655a1bc9abafcb62
Rafal Urniaz <RafalUrniaz@users.noreply.github.com>
2018-11-09 20:22:08 +0100

    update

--- HeatMap4kmeRs/matplotlib_heatmap_functions.py
+++ HeatMap4kmeRs/matplotlib_heatmap_functions.py
@@ -1,136 +1,18 @@
 # (C) Rafal Urniaz
 #  
 
-# Import required modules
-import os, csv, sys
-
 # Data 
 import numpy as np
-import pandas as pd
 
 # Graphics 
 import matplotlib
 import matplotlib.pyplot as plt
 
-
-# Function takes as an argument the filename and directory 
-# and returns pandas' dataframe
-
-def read_file(filename = ""):
-
-    if os.path.isfile(filename) and os.access(filename, os.R_OK):
-        file_dataframe = pd.read_csv(filename, sep = ";", index_col = 0)
-        file_dataframe = pd.DataFrame(file_dataframe.iloc[1:len(file_dataframe.iloc[0])], dtype = "float_")
-        print("File exists and is readable [-- OK --]")
-
-        # Print first 5 rows
-        print(file_dataframe.iloc[0:3,])
-    
-    else:
-        print("Either the file is missing or not readable")
-        sys.exit()
-    
-    return file_dataframe
-
-
-# Prepare data function takes as an argument the read_file() function
-# output and returns list of dataframe
-
-def prepare_data(file_dataframe):
-
-    # x axis labels - columns names
-    x_labels = file_dataframe.columns
-
-    # y axis label - rows names
-    y_labels = file_dataframe.index
-
-    # data to the heatmap plot
-    data = file_dataframe.values
-
-    return [x_labels, y_labels, data]
-
-
-# Save heatmap in location defined by filename 
-
-def save_or_show_heatmap(plt, show = True, file_name = ""):
-
-    # Show = True
-    if show == True:
-        plt.show()
-    if show == False:
-        plt.savefig(file_name)
-    return ""
-
-
-# Prepare data function takes as an argument the read_file() function
-# output and returns list of dataframe
-
-def quick_kmeRs_heatmap(file_dataframe, title = "Example GATTACA HeatMap", show = True, file_name = ""):
-
-# Prepare data
-    
-    x = prepare_data(file_dataframe)
-
-    x_labels = x[0]
-    y_labels = x[1]
-    data = x[2]
-
-# Prepare HeatMap
-
-    fig, ax = plt.subplots()
-    im = ax.imshow(data)
-
-# -- Lablels --
-
-    # Show all ticks
-    ax.set_xticks(np.arange(len(x_labels)))
-    ax.set_yticks(np.arange(len(y_labels)))
-
-    # Label with the respective list entries
-    ax.set_xticklabels(x_labels)
-    ax.set_yticklabels(y_labels)
-
-    # Rotate 45 degrees the labels and set their alignment
-    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",rotation_mode="anchor")
-
-    # Loop over data dimensions and create text annotations.
-    for i in range(len(y_labels)):
-        for j in range(len(x_labels)):
-            ax.text(j, i, data[i, j],ha="center", va="center", color="w")
-
-    ax.set_title(title)
-
-
-    fig.tight_layout()
-
-# Save or show the plot
-
-    save_or_show_heatmap(plt, show, file_name)
-
-    return "Done!"
-
-
-# Predefine 1
-
-def heatmap_var1(file_dataframe):
-
-# Prepare data
-    x = prepare_data(file_dataframe)
-    x_labels = x[0]
-    y_labels = x[1]
-    data = x[2]
-    
-    fig, ax = plt.subplots()
-
-    im, cbar = heatmap(data, y_labels, x_labels, ax=ax, cmap="YlGn", cbarlabel="harvest [t/year]")
-    texts = annotate_heatmap(im, valfmt="{x:.1f} t")
-
-    fig.tight_layout()
-    plt.show()
-
+# General matplotlib HeatMap Functions
 
 def heatmap(data, row_labels, col_labels, ax=None,
             cbar_kw={}, cbarlabel="", **kwargs):
+
     """
     Create a heatmap from a numpy array and two lists of labels.
 
--- HeatMap4kmeRs/test_script_for_package.py
+++ HeatMap4kmeRs/test_script_for_package.py
@@ -4,9 +4,9 @@
 # Import modules
 
 try:
-    from heatmap import kmeRs_heatmap
-    from heatmap import quick_kmeRs_heatmap
-    from heatmap import read_file
+    from kmeRs_annotated_heatmap import quick_kmeRs_heatmap
+    from kmeRs_annotated_heatmap import read_file
+    #import matplotlib_heatmap_functions
     print("import: [-- OK --]")
 except ImportError:
     print("Could not import heatmap4kmers package")
@@ -18,4 +18,4 @@ x = read_file(filename = r"example_data\example_kmeRs_matrix.csv")
 # Prepare the Quick HeatMap
 quick_kmeRs_heatmap(file_dataframe = x)
 
-kmeRs_heatmap(file_dataframe = x)
+
