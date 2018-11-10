c7ef80ac0d15de0ad2cdde7760f01dce9c6ff762
Rafal Urniaz <RafalUrniaz@users.noreply.github.com>
2018-11-10 17:41:05 +0100

    update

--- HeatMap4kmeRs/test_script_for_package.py
+++ HeatMap4kmeRs/test_script_for_package.py
@@ -23,10 +23,10 @@ x = read_file(filename = r"example_data\example_kmeRs_matrix.csv")
 # Prepare the Quick HeatMap Demo
 
 # Standard
-kmeRs_annotated_heatmap(file_dataframe = x, show_legend = False)
+#kmeRs_annotated_heatmap(file_dataframe = x, show_legend = False)
 
 # Categorised
-kmeRs_annotated_heatmap(file_dataframe = x, cmap = plt.cm.get_cmap('Blues', 10))
+#kmeRs_annotated_heatmap(file_dataframe = x, cmap = plt.cm.get_cmap('Blues', 10))
 
 # RdBu positive vs. negative values
 kmeRs_annotated_heatmap(file_dataframe = x, cmap="RdBu")
