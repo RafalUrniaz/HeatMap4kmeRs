dc447c862be2c8170db14e16e4e37b906f748935
Rafal Urniaz <RafalUrniaz@users.noreply.github.com>
2018-11-12 05:45:55 +0100

    add

--- docs/conf.py
+++ docs/conf.py
@@ -14,9 +14,9 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../..'))
+print(sys.path.insert(0, os.path.abspath('../..')))
 print(sys.path)
-
 # -- Project information -----------------------------------------------------
 
 project = 'HeatMap4kmeRs'
