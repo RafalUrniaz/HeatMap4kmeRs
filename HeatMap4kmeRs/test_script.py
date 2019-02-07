e59fa48dd40d134baec6d4cc349b883d30774b70
Rafal Urniaz <RafalUrniaz@users.noreply.github.com>
2019-02-07 21:03:44 +0100

    Update test_script.py

--- HeatMap4kmeRs/test_script.py
+++ HeatMap4kmeRs/test_script.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 """Test script module for heatmap4kmers package
 
-This module demonstrates the example charts for kmeRs package
+Test module demonstrates the example charts for kmeRs package 
+and checks the package integrity
 
 """
 ## (C) Rafal Urniaz
@@ -23,17 +24,19 @@ except ImportError:
 if __name__ == '__main__':
 
 # --- Import sample_file 
-
-    x = read_file(filename = r"example_data\example_kmeRs_matrix.csv")
-
+    try:
+        x = read_file(filename = r"example_data\example_kmeRs_matrix.csv")
+        print("Load demo dataframe [-- OK --]")
 # --- Prepare Quick HeatMap Demo
 
 # Standard
-    #kmers_heatmap(file_dataframe = x, show_legend = False)
-
+        kmers_heatmap(file_dataframe = x, show_legend = False, save_file = True, file_name = "Figure_1.png")
+        print("Save demo plot as Figure_1.png [-- OK --]")
 # Categorised
-    #kmers_heatmap(file_dataframe = x, cmap = plt.cm.get_cmap('Blues', 10), save_file = True, file_name = "Figure_2")
-
+        kmers_heatmap(file_dataframe = x, cmap = plt.cm.get_cmap('Blues', 10), save_file = True, file_name = "Figure_2.png")
+        print("Save demo plot as Figure_2.png [-- OK --]")
 # RdBu positive vs. negative values
-    kmers_heatmap(file_dataframe = x[x.columns.difference(["score_total"])], cmap="RdBu")
-
+        kmers_heatmap(file_dataframe = x[x.columns.difference(["score_total"])], cmap="RdBu", save_file = True, file_name = "Figure_3.png")
+        print("Save demo plot as Figure_3.png [-- OK --]")
+    except:
+        print("Test not pass [-- Error --]")
